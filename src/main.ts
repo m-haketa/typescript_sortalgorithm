578f30d8e476eb1639a78d2ea95cf3bd27d25dbc
m-haketa <git@excelspeedup.com>
2019-06-25 23:10:19 +0900

    移動時色づけ、コンポーネント名変更

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import MyComponent from './sort'
+import BubbleSort from './sort'
 
 Vue.config.productionTip = false
 
 let vm = new Vue({
   el: '#app',
   components: {
-    'my-component': MyComponent
+    'bubble-sort': BubbleSort
   }
 })
--- src/sort.ts
+++ src/sort.ts
@@ -4,13 +4,13 @@ import Component from 'vue-class-component'
 @Component({
   template: `
     <div id="list-complete-demo" class="demo">
-      <button v-on:click="shuffle">Shuffle</button>
-      <button v-on:click="sort">Sort</button>
+      <button v-on:click="start">Start</button>
       <button v-on:click="stop">Stop</button>
       <transition-group name="list-complete" tag="p">
         <span
-          v-for="item in items"
+          v-for="(item,index) in items"
           v-bind:key="item"
+          v-bind:class="{moving: isMoving(index)}"
           class="list-complete-item"
         >
           {{ item }}
@@ -19,19 +19,26 @@ import Component from 'vue-class-component'
     </div>
   `
 })
-export default class MyComponent extends Vue {
+export default class BubbleSort extends Vue {
   items = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-  nextNum = 10
+  itemMoving = [-1, -1]
   doSort = false
   sortLogic = this.sortImpl()
 
-  shuffle() {
+  init() {
+    this.sortLogic = this.sortImpl()
+    this.itemMoving = [-1, -1]
     const _ = require('underscore')
     this.items = _.shuffle(this.items)
   }
 
   stop() {
     this.doSort = false
+  }
+
+  start() {
+    this.init()
+    this.sort()
   }
 
   sort() {
@@ -43,19 +50,24 @@ export default class MyComponent extends Vue {
     }, 1000)
   }
 
+  isMoving(no: number) {
+    return no === this.itemMoving[0] || no === this.itemMoving[1]
+  }
+
   *sortImpl() {
     for (let i = 0; i < this.items.length; i++) {
       for (let j = this.items.length - 1; j > i; j--) {
-        console.log(this.items, i, j)
-        yield true
-
+        this.itemMoving = [j - 1, j]
         if (this.items[j - 1] > this.items[j]) {
           const swpTempArr = this.items.splice(j - 1, 1, this.items[j])
           this.items.splice(j, 1, swpTempArr[0])
         }
+
+        yield true
       }
     }
 
+    this.itemMoving = [-1, -1]
     while (true) {
       yield false
     }
